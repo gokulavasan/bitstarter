ab2c7cf73655c49e534c88e8fa08d0ccb87ed411
Gokul Gunasekaran <gokulavasan@gmail.com>
2013-07-17 07:19:32 +0000

    new files

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buf = new Buffer(32);
+buf.write(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
